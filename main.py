dcfb3d0852465aa47dc939ead52d0e1633a0dfd8
Slekatom <nemo.bma@gmail.com>
2024-10-10 11:15:19 +0300

    second commit

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ display.set_caption("Battle City")
 clock = time.Clock()
 FPS = 60
 
-mixer.init()
-mixer.music.load('War.mp3')
-mixer.music.set_volume(0.9)
-mixer.music.play(0)
+# mixer.init()
+# mixer.music.load('War.mp3')
+# mixer.music.set_volume(0.9)
+# mixer.music.play(0)
 
 class Game: # Клас гри в нього додаємо усі об'єкти класів (по типу контроллера)
     def __init__(self, scene, player, enemies, map):
@@ -28,7 +28,7 @@ class Game: # Клас гри в нього додаємо усі об'єкти 
         clock.tick(FPS)
         display.update()        
 
-class Object(ABC): # Абстрактний клас забов'язує прилеглі класи рухатися
+class Enity(ABC): # Абстрактний клас забов'язує прилеглі класи рухатися
     @abstractmethod 
     def move(self, x, y):
         pass
@@ -43,7 +43,7 @@ class NotMoving(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(Object, sprite.Sprite): 
+class Player(Enity, sprite.Sprite): 
     def __init__(self, x, y, hp, score, speed, size):
         sprite.Sprite.__init__(self)  
         self.hp = hp  
@@ -121,16 +121,13 @@ class Player(Object, sprite.Sprite):
 class Scene():  # Сцена 
     pass
 
-class EnemyFactory(): 
-    def create_enemy(self, enemy_type, x, y, damage, speed, patrol_route, image, size):
-        if enemy_type == 'A':
-            return EnemyTypeA(x, y, damage, speed, patrol_route, image, size)
-        else:
-            raise ValueError(f"Unknown enemy type: {enemy_type}")
+
 class EnemyTypeA(sprite.Sprite):
     def __init__(self, x, y, damage, speed, patrol_route, enemy_image, size):
         super().__init__()
-        self.original_image = transform.scale(image.load("player.png"), size)  #оригінальне незмінне фото (потрібно для обертання самого зображення)
+        self.size = size
+        self.enemy_image = enemy_image
+        self.original_image = transform.scale(image.load("player.png"), self.size)  #оригінальне незмінне фото (потрібно для обертання самого зображення)
         self.image = self.original_image.copy()  
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -149,44 +146,52 @@ class EnemyTypeA(sprite.Sprite):
             self.image = transform.rotate(self.original_image, 90)
         elif self.diraction == "right":
             self.image = transform.rotate(self.original_image, -90)
+        
         elif self.diraction == "up":
-            self.image = self.original_image.copy() 
+            self.image = transform.rotate(self.original_image, 0)
         elif self.diraction == "down":
             self.image = transform.rotate(self.original_image, 180)
     
-        self.rect = self.image.get_rect(center=self.rect.center)
+        #self.rect = self.image.get_rect(center=self.rect.center)
 
     def move(self):
-        """Переміщення ворога по маршруту патруля."""
-        moving_ind = 0
-        for i in self.patrol_route:
-            target_x, target_y = self.patrol_route[self.current_target]
+        target_x, target_y, diraction = self.patrol_route[self.current_target]
 
-            # хлдьба до поточної цили
-            if moving_ind == 1:
-                if self.rect.x == target_x:
-                    moving_ind = 0
-                if self.rect.x < target_x:
-                    self.rect.x += self.speed + 3
-                    self.diraction = "right"
-                elif self.rect.x > target_x:
-                    self.rect.x -= self.speed + 3
-                    self.diraction = "left"
-            if moving_ind == 0:
-                if self.rect.y == target_y:
-                    moving_ind = 1
-                if self.rect.y < target_y:
-                    self.rect.y += self.speed
-                    self.diraction = "down"
-                elif self.rect.y > target_y:
-                    self.rect.y -= self.speed 
-                    self.diraction = "up"
+        # Рух по горизонталі
+        self.diraction = diraction
+        if self.rect.x < target_x:
+            self.rect.x += self.speed
+            
+        elif self.rect.x > target_x:
+            self.rect.x -= self.speed
+            
 
-            # Якщо досягли цілі, перемикаємось на наступну точку
-            if self.rect.x == target_x and self.rect.y == target_y:
-                self.current_target = (self.current_target + 1) % len(self.patrol_route)
-            self.rotate_image()
-            print(f"Enemy moved to ({self.rect.x}, {self.rect.y})")
+        # Рух по вертикалі, тільки якщо x досяг цільового
+        if abs(self.rect.x - target_x) <= self.speed:
+            if self.rect.y < target_y:
+                self.rect.y += self.speed
+                
+            elif self.rect.y > target_y:
+                self.rect.y -= self.speed
+        self.rotate_image()
+        print(self.rect.x, self.rect.y)
+    # Якщо досягнуто ціль, переходимо до наступної точки
+        if abs(self.rect.x - target_x) <= self.speed and abs(self.rect.y - target_y) <= self.speed:
+            self.current_target = (self.current_target + 1) % len(self.patrol_route)
+            print("Я досяг цілі ", self.rect.x, self.rect.y)
+            
+        # Після руху оновлюємо зображення
+        
+        # Після руху оновлюємо зображення в залежності від напрямку
+
+class EnemyFactory(): 
+    def create_enemy(self, enemy_type, x, y, enemy: EnemyTypeA):
+        if enemy_type == 'A':
+            enemy.rect.x = x
+            enemy.rect.y = y
+            return EnemyTypeA(enemy.rect.x, enemy.rect.y, enemy.damage, enemy.speed, enemy.patrol_route, enemy.enemy_image, enemy.size)
+        else:
+            raise ValueError(f"Unknown enemy type: {enemy_type}")
 
 class Shoot():  # Класс для шмаляння
     pass
@@ -199,16 +204,50 @@ game_back_ground = NotMoving("background.jpg", 0, 0, (1200, 700))
 
 player = Player(100, 100, 100, 0, 5, (50, 90))
 
-wall1 = NotMoving("wall.jpg", 200, 300, (50, 200))
-wall2 = NotMoving("wall.jpg", 350, 300, (50, 200))
+wall1 = NotMoving("wall.jpg", 200, 600, (50, 250))
+wall2 = NotMoving("wall.jpg", 0, 450, (300, 50))
+wall3 = NotMoving("wall.jpg", 150, 450-200, (50, 200))
+wall4 = NotMoving("wall.jpg", 450, 400, (50, 300))
+wall5 = NotMoving("wall.jpg", 650, 550, (250, 50))
+wall6 = NotMoving("wall.jpg", 650, 550-300, (50, 300))
+wall7 = NotMoving("wall.jpg", 550-100, 250, (200, 50))
+wall8 = NotMoving("wall.jpg", 550-100, 0, (50, 150))
+wall9 = NotMoving("wall.jpg", 950, 0, (50, 150))
+wall10 = NotMoving("wall.jpg", 1200-300, 350, (300, 50))
 
 walls = sprite.Group()
 walls.add(wall1)
 walls.add(wall2)
+walls.add(wall3)
+walls.add(wall4)
+walls.add(wall5)
+walls.add(wall6)
+walls.add(wall7)
+walls.add(wall8)
+walls.add(wall9)
+walls.add(wall10)
 
+patrol_route = [
+    (100, 100, "up"),  # Початкова точка
+    (300, 100, "right"),  # Перша ціль (рух праворуч)
+    (300, 180, "down"),  # Друга ціль (рух вниз)
+    (800, 200, "right"),  # Третя ціль (рух ліворуч)
+    (800, 455, "down"),   # Повернення до початкової точки
+    (1000, 455, "right"), 
+    (1000, 650, "down"), 
+    (550, 650, "left"),
+    (550, 300, "up") ,
+    (300, 300, "left") ,
+    (300, 100, "up") ,
+    (100, 100, "left") ,
+    (100, 300, "down") 
+    
+]
 enemy_creator = EnemyFactory()
-enemy_type_A = EnemyTypeA(300, 400, 10, 1, [(2, 2), (10, 10), (2, 10)], "player.png", (50, 90))
-enemy1 = enemy_creator.create_enemy('A', 200, 600, 10, 1, [(200, 600), (200, 100), (1000, 100), (1000, 600)], "player.png", (50, 90))
+enemy_type_A = EnemyTypeA(100, 300, 4, 5, patrol_route , "player.png", (50, 90))
+enemy1 = enemy_creator.create_enemy('A', 100, 300, enemy_type_A)
+enemy2 = enemy_creator.create_enemy('A', 100, 100, enemy_type_A)
+
 
 while game.game == True:
     for e in event.get():
@@ -220,8 +259,19 @@ while game.game == True:
     player.move(walls)
     wall1.reset()
     wall2.reset()
+    wall3.reset()
+    wall4.reset()
+    wall5.reset()
+    wall6.reset()
+    wall7.reset()
+    wall8.reset()
+    wall9.reset()
+    wall10.reset()
+
     enemy1.reset()
     enemy1.move()
+    enemy2.reset()
+    enemy2.move()
     
     
     game.update()
